04aaeba2d9f707237f43450647a1826408d26ca2
Kendrick Andrew J. Cosca <201511656@fit-comlab.edu.ph>
2019-06-11 17:33:02 +0800

    Offline Version

--- codeigniter/application/views/templates/header.php
+++ codeigniter/application/views/templates/header.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $page_title ?></title>
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="<?= base_url("css/nunito.css"); ?>">
+    <link rel="stylesheet" href="<?= base_url("css/all.min.css"); ?>">
     <link rel="stylesheet" href="<?= base_url("css/bootstrap.min.css"); ?>">
     <link rel="stylesheet" href="<?= base_url("css/bootstrap-select.css") ?>" />
     <link rel="stylesheet" href="<?= base_url("css/custom.css"); ?>">
